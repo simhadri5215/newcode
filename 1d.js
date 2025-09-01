6b33e9d3ca023412070e66900d7dc4ccc14c5f38
simhadri5215 <23kb1a3040@nbkrist.org>
2025-09-01 15:31:58 +0530

    Add files via upload

---
+++ 1d.js
@@ -0,0 +1,13 @@
+// Select buttons by their IDs
+const button1 = document.getElementById('btn1');
+const button2 = document.getElementById('btn2');
+
+// Attach click event listener to Button 1
+button1.addEventListener('click', function() {
+  alert('Button 1 clicked!');
+});
+
+// Attach click event listener to Button 2
+button2.addEventListener('click', function() {
+  alert('Button 2 clicked!');
+});
